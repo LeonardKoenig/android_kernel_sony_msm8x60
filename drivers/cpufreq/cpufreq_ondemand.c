9ef0344411af14618b75bcb3fb39ee7a9ff59c70
Brian Steuer <bsteuer@codeaurora.org>
2010-11-05 01:59:23 -0700

    cpufreq: Make sure input handler always runs on cpu0
    Make sure that only a single input handle is registered for the
    ondemand governor. Force the input handler to run on cpu0 so it
    will not be migrated due to cpu hotplug.
    
    Change-Id: Ic72c34355c629da01290cf1a5e81d534aaf83d86
    Signed-off-by: Brian Steuer <bsteuer@codeaurora.org>

--- drivers/cpufreq/cpufreq_ondemand.c
+++ drivers/cpufreq/cpufreq_ondemand.c
@@ -981,7 +981,7 @@ static DECLARE_WORK(dbs_refresh_work, dbs_refresh_callback);
 static void dbs_input_event(struct input_handle *handle, unsigned int type,
 		unsigned int code, int value)
 {
-	schedule_work(&dbs_refresh_work);
+	schedule_work_on(0, &dbs_refresh_work);
 }
 
 static int dbs_input_connect(struct input_handler *handler,
@@ -1098,7 +1098,8 @@ static int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 			if (dbs_tuners_ins.sync_freq == 0)
 				dbs_tuners_ins.sync_freq = policy->min;
 		}
-		rc = input_register_handler(&dbs_input_handler);
+		if (!cpu)
+			rc = input_register_handler(&dbs_input_handler);
 		mutex_unlock(&dbs_mutex);
 
 
@@ -1114,7 +1115,8 @@ static int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 
 		mutex_lock(&dbs_mutex);
 		dbs_enable--;
-		input_unregister_handler(&dbs_input_handler);
+		if (!cpu)
+			input_unregister_handler(&dbs_input_handler);
 		mutex_unlock(&dbs_mutex);
 		if (!dbs_enable)
 			sysfs_remove_group(cpufreq_global_kobject,

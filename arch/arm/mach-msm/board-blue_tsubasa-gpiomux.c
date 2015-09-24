677f322ffe5efcab96073318d46aedd328073568
Leonard König <leonard.r.koenig@googlemail.com>
2015-09-24 20:00:49 +0200

    import 9.2.A.0.295
    Change-Id: I1e7f5026e88cb89b791784218ace3dfd155661db

--- arch/arm/mach-msm/board-blue_tsubasa-gpiomux.c
+++ arch/arm/mach-msm/board-blue_tsubasa-gpiomux.c
@@ -1,5 +1,4 @@
-/* Copyright (c) 2011-2012, Code Aurora Forum. All rights reserved.
- * Copyright (C) 2012 Sony Mobile Communications AB.
+/* Copyright (c) 2012, Code Aurora Forum. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -12,12 +11,19 @@
  *
  */
 
+#include <linux/gpio.h>
 #include <asm/mach-types.h>
-#include <mach/gpio.h>
 #include <mach/gpiomux.h>
 #include <mach/socinfo.h>
 #include "devices.h"
 #include "board-8960.h"
+
+static struct gpiomux_setting unused_gpio = {
+	.func = GPIOMUX_FUNC_GPIO,
+	.drv  = GPIOMUX_DRV_2MA,
+	.pull = GPIOMUX_PULL_NONE,
+	.dir  = GPIOMUX_OUT_LOW,
+};
 
 static struct gpiomux_setting gpio_2ma_no_pull_low = {
 	.func = GPIOMUX_FUNC_GPIO,
@@ -139,25 +145,10 @@ static struct gpiomux_setting debug_uart_rx = {
 	.pull = GPIOMUX_PULL_UP
 };
 
-static struct gpiomux_setting wcnss_5wire_suspend_cfg = {
-	.func = GPIOMUX_FUNC_GPIO,
-	.drv  = GPIOMUX_DRV_2MA,
-	.pull = GPIOMUX_PULL_UP,
-};
-
-static struct gpiomux_setting wcnss_5wire_active_cfg = {
-	.func = GPIOMUX_FUNC_1,
-	.drv  = GPIOMUX_DRV_6MA,
-	.pull = GPIOMUX_PULL_DOWN,
-};
-
-static struct msm_gpiomux_config semc_viskan_all_cfgs[] __initdata = {
-	{ /* RESET_STATUS_N */
+static struct msm_gpiomux_config semc_blue_all_cfgs[] __initdata = {
+	{ /* NC */
 		.gpio = 0,
-		.settings = {
-			[GPIOMUX_ACTIVE] = &gpio_2ma_no_pull_in,
-			[GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_in,
-		},
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
 	{ /* MCAM_RST_N */
 		.gpio = 1,
@@ -194,24 +185,21 @@ static struct msm_gpiomux_config semc_viskan_all_cfgs[] __initdata = {
 			[GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low,
 		},
 	},
-	{ /* TP_RESET_N */
+	{ /* NC */
 		.gpio = 6,
-		.settings = {
-			[GPIOMUX_ACTIVE] = &gpio_2ma_no_pull_high,
-			[GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_high,
-		},
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
 	{ /* NC */
 		.gpio = 7,
-		.settings = { [GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low, },
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
 	{ /* NC */
 		.gpio = 8,
-		.settings = { [GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low, },
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
 	{ /* NC */
 		.gpio = 9,
-		.settings = { [GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low, },
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
 	{ /* ACCEL_INT */
 		.gpio = 10,
@@ -223,8 +211,8 @@ static struct msm_gpiomux_config semc_viskan_all_cfgs[] __initdata = {
 	{ /* TP_INT */
 		.gpio = 11,
 		.settings = {
-			[GPIOMUX_ACTIVE] = &gpio_2ma_pull_up_in,
-			[GPIOMUX_SUSPENDED] = &gpio_2ma_pull_up_in,
+			[GPIOMUX_ACTIVE] = &gpio_2ma_no_pull_in,
+			[GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_in,
 		},
 	},
 	{ /* HW_ID0 */
@@ -272,8 +260,8 @@ static struct msm_gpiomux_config semc_viskan_all_cfgs[] __initdata = {
 	{ /* CHAT_CAM_RST_N */
 		.gpio = 18,
 		.settings = {
-			[GPIOMUX_ACTIVE] = &gpio_2ma_no_pull_low,
-			[GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low,
+			[GPIOMUX_ACTIVE] = &gpio_2ma_no_pull_high,
+			[GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_high,
 		},
 	},
 	{ /* NFC_DWLD_EN */
@@ -299,36 +287,30 @@ static struct msm_gpiomux_config semc_viskan_all_cfgs[] __initdata = {
 	},
 	{ /* NC */
 		.gpio = 22,
-		.settings = { [GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low, },
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
 	{ /* NC */
 		.gpio = 23,
-		.settings = { [GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low, },
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
 	{ /* NC */
 		.gpio = 24,
-		.settings = { [GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low, },
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
 	{ /* NC */
 		.gpio = 25,
-		.settings = { [GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low, },
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
 
 	/* NOT CONFIGURED: 26-31 */
-	/* 26: FM_SSBI (Follow QCT) */
-	/* 27: FM_DATA (Follow QCT) */
-	/* 28: BT_CTL (Follow QCT) */
-	/* 29: BT_DATA (Follow QCT) */
-	/* 30: UIM1_DATA_MSM (Follow QCT) */
-	/* 31: UIM1_CLK_MSM (Follow QCT) */
 
 	{ /* NC */
 		.gpio = 32,
-		.settings = { [GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low, },
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
 	{ /* NC */
 		.gpio = 33,
-		.settings = { [GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low, },
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
 	{ /* UART_TX_DFMS */
 		.gpio = 34,
@@ -360,27 +342,27 @@ static struct msm_gpiomux_config semc_viskan_all_cfgs[] __initdata = {
 	},
 	{ /* NC */
 		.gpio = 38,
-		.settings = { [GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low, },
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
 	{ /* NC */
 		.gpio = 39,
-		.settings = { [GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low, },
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
 	{ /* NC */
 		.gpio = 40,
-		.settings = { [GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low, },
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
 	{ /* NC */
 		.gpio = 41,
-		.settings = { [GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low, },
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
 	{ /* NC */
 		.gpio = 42,
-		.settings = { [GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low, },
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
 	{ /* NC */
 		.gpio = 43,
-		.settings = { [GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low, },
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
 	{ /* I2C_DATA_SENS */
 		.gpio = 44,
@@ -410,12 +392,9 @@ static struct msm_gpiomux_config semc_viskan_all_cfgs[] __initdata = {
 			[GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low,
 		},
 	},
-	{ /* GYRO_FSYNC */
+	{ /* NC */
 		.gpio = 48,
-		.settings = {
-			[GPIOMUX_ACTIVE] = &gpio_2ma_no_pull_low,
-			[GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low,
-		},
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
 	{ /* PROX_INT_N */
 		.gpio = 49,
@@ -426,7 +405,7 @@ static struct msm_gpiomux_config semc_viskan_all_cfgs[] __initdata = {
 	},
 	{ /* NC */
 		.gpio = 50,
-		.settings = { [GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low, },
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
 	{ /* USB_OTG_EN */
 		.gpio = 51,
@@ -437,18 +416,15 @@ static struct msm_gpiomux_config semc_viskan_all_cfgs[] __initdata = {
 	},
 	{ /* NC */
 		.gpio = 52,
-		.settings = { [GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low, },
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
 	{ /* NC */
 		.gpio = 53,
-		.settings = { [GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low, },
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
-	{ /* LMU_HW_EN */
+	{ /* NC */
 		.gpio = 54,
-		.settings = {
-			[GPIOMUX_ACTIVE] = &gpio_2ma_no_pull_high,
-			[GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_high,
-		},
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
 	{ /* NC */
 		.gpio = 55,
@@ -460,28 +436,24 @@ static struct msm_gpiomux_config semc_viskan_all_cfgs[] __initdata = {
 	},
 
 	/* 57 not configured */
-	/* 57: WTRO_APT_EN (Follow QCT) */
 
-	{ /* BMU_INT_N */
+	{ /* NC */
 		.gpio = 58,
-		.settings = {
-			[GPIOMUX_ACTIVE] = &gpio_2ma_pull_up_in,
-			[GPIOMUX_SUSPENDED] = &gpio_2ma_pull_up_in,
-		},
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
-	{ /* SLIMBUS1_MCLK (Follow QCT) */
+	{ /* SLIMBUS1_MCLK */
 		.gpio = 59,
 		.settings = {
 			[GPIOMUX_SUSPENDED] = &cdc_mclk,
 		},
 	},
-	{ /* SLIMBUS1_CLK (Follow QCT) */
+	{ /* SLIMBUS1_CLK */
 		.gpio = 60,
 		.settings = {
 			[GPIOMUX_SUSPENDED] = &slimbus,
 		},
 	},
-	{ /* SLIMBUS1_DATA (Follow QCT) */
+	{ /* SLIMBUS1_DATA */
 		.gpio = 61,
 		.settings = {
 			[GPIOMUX_SUSPENDED] = &slimbus,
@@ -489,17 +461,16 @@ static struct msm_gpiomux_config semc_viskan_all_cfgs[] __initdata = {
 	},
 
 	/* 62 not configured */
-	/* 62: CODEC_MAD_INT_N (Follow QCT) */
 
 	{ /* NC */
 		.gpio = 63,
-		.settings = { [GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low, },
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
 	{ /* MHL_RST_N */
 		.gpio = 64,
 		.settings = {
-			[GPIOMUX_ACTIVE] = &gpio_2ma_no_pull_high,
-			[GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_high,
+			[GPIOMUX_ACTIVE] = &gpio_2ma_no_pull_low,
+			[GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low,
 		},
 	},
 	{ /* MHL_INT */
@@ -518,11 +489,11 @@ static struct msm_gpiomux_config semc_viskan_all_cfgs[] __initdata = {
 	},
 	{ /* NC */
 		.gpio = 67,
-		.settings = { [GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low, },
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
 	{ /* NC */
 		.gpio = 68,
-		.settings = { [GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low, },
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
 	{ /* GYRO_INT_N */
 		.gpio = 69,
@@ -540,11 +511,11 @@ static struct msm_gpiomux_config semc_viskan_all_cfgs[] __initdata = {
 	},
 	{ /* NC */
 		.gpio = 71,
-		.settings = { [GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low, },
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
 	{ /* NC */
 		.gpio = 72,
-		.settings = { [GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low, },
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
 	{ /* I2C_DATA_PERI */
 		.gpio = 73,
@@ -562,82 +533,48 @@ static struct msm_gpiomux_config semc_viskan_all_cfgs[] __initdata = {
 	},
 	{ /* NC */
 		.gpio = 75,
-		.settings = { [GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low, },
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
 	{ /* NC */
 		.gpio = 76,
-		.settings = { [GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low, },
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
 	{ /* NC */
 		.gpio = 77,
-		.settings = { [GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low, },
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
-	{ /* NC (TP_SWIO)*/
+	{ /* NC */
 		.gpio = 78,
-		.settings = { [GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low, },
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
 	{ /* NC */
 		.gpio = 79,
-		.settings = { [GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low, },
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
 	{ /* NC */
 		.gpio = 80,
-		.settings = { [GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low, },
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
 	{ /* NC */
 		.gpio = 81,
-		.settings = { [GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low, },
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
 	{ /* NC */
 		.gpio = 82,
-		.settings = { [GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low, },
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
 
 	/* 83 not configured */
-	/* 83: BT_SSBI (Follow QCT) */
 
-	{/* WL_CMD_DATA2 (Follow QCT) */
-		.gpio = 84,
-		.settings = {
-			[GPIOMUX_ACTIVE]    = &wcnss_5wire_active_cfg,
-			[GPIOMUX_SUSPENDED] = &wcnss_5wire_suspend_cfg,
-		},
-	},
-	{/* WL_CMD_DATA1 (Follow QCT) */
-		.gpio = 85,
-		.settings = {
-			[GPIOMUX_ACTIVE]    = &wcnss_5wire_active_cfg,
-			[GPIOMUX_SUSPENDED] = &wcnss_5wire_suspend_cfg,
-		},
-	},
-	{/* WL_CMD_DATA0 (Follow QCT) */
-		.gpio = 86,
-		.settings = {
-			[GPIOMUX_ACTIVE]    = &wcnss_5wire_active_cfg,
-			[GPIOMUX_SUSPENDED] = &wcnss_5wire_suspend_cfg,
-		},
-	},
-	{/* WL_CMD_SET (Follow QCT) */
-		.gpio = 87,
-		.settings = {
-			[GPIOMUX_ACTIVE]    = &wcnss_5wire_active_cfg,
-			[GPIOMUX_SUSPENDED] = &wcnss_5wire_suspend_cfg,
-		},
-	},
-	{/* WL_CMD_CLK (Follow QCT) */
-		.gpio = 88,
-		.settings = {
-			[GPIOMUX_ACTIVE]    = &wcnss_5wire_active_cfg,
-			[GPIOMUX_SUSPENDED] = &wcnss_5wire_suspend_cfg,
-		},
-	},
+	/* 84-88 configured in separate struct below */
+
 	{ /* NC */
 		.gpio = 89,
-		.settings = { [GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low, },
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
 	{ /* NC */
 		.gpio = 90,
-		.settings = { [GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low, },
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
 	{ /* GSM_TX_MASK */
 		.gpio = 91,
@@ -646,39 +583,37 @@ static struct msm_gpiomux_config semc_viskan_all_cfgs[] __initdata = {
 			[GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low,
 		},
 	},
-
-	{ /* NC (ANT_TUNE0)*/
+	{ /* NC */
 		.gpio = 92,
-		.settings = { [GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low, },
+		.settings = { [GPIOMUX_SUSPENDED] = &gpio_2ma_pull_down_in, },
 	},
-
 	{ /* NC */
 		.gpio = 93,
-		.settings = { [GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low, },
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
 	{ /* NC */
 		.gpio = 94,
-		.settings = { [GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low, },
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
 	{ /* NC */
 		.gpio = 95,
-		.settings = { [GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low, },
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
 	{ /* NC */
 		.gpio = 96,
-		.settings = { [GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low, },
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
 	{ /* NC */
 		.gpio = 97,
-		.settings = { [GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low, },
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
 	{ /* NC */
 		.gpio = 98,
-		.settings = { [GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low, },
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
 	{ /* NC */
 		.gpio = 99,
-		.settings = { [GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low, },
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
 	{ /* HDMI_DDCSCL */
 		.gpio = 100,
@@ -694,7 +629,7 @@ static struct msm_gpiomux_config semc_viskan_all_cfgs[] __initdata = {
 			[GPIOMUX_SUSPENDED] = &hdmi_suspend_1_cfg,
 		},
 	},
-	{ /* HDMI_HPD (Follow QCT) */
+	{ /* HDMI_HPD */
 		.gpio = 102,
 		.settings = {
 			[GPIOMUX_ACTIVE]    = &hdmi_active_2_cfg,
@@ -703,9 +638,6 @@ static struct msm_gpiomux_config semc_viskan_all_cfgs[] __initdata = {
 	},
 
 	/* 103-105 not configured */
-	/* 103: PM_APC_SEC_IRQ_N (Follow QCT) */
-	/* 104: PM_APC_USR_IRQ_N (Follow QCT) */
-	/* 105: PM_MDM_IRQ_N (Follow QCT) */
 
 	{ /* NFC_IRQ */
 		.gpio = 106,
@@ -723,7 +655,6 @@ static struct msm_gpiomux_config semc_viskan_all_cfgs[] __initdata = {
 	},
 
 	/* 108 not configured */
-	/* 108: PS_HOLD (Follow QCT) */
 
 	{ /* NC */
 		.gpio = 109,
@@ -735,15 +666,6 @@ static struct msm_gpiomux_config semc_viskan_all_cfgs[] __initdata = {
 	},
 
 	/* 111-119 not configured */
-	/* 111: PRX_SW_SEL/WDOG_DISABLE (Follow QCT) */
-	/* 112: ANT_SW_SEL4 (Follow QCT) */
-	/* 113: DRX_MODE_SEL2 (Follow QCT) */
-	/* 114: DRX_MODE_SEL1 (Follow QCT) */
-	/* 115: DRX_MODE_SEL0 (Follow QCT) */
-	/* 116: ANT_SW_SEL3 (Follow QCT) */
-	/* 117: ANT_SW_SEL2 (Follow QCT) */
-	/* 118: ANT_SW_SEL1 (Follow QCT) */
-	/* 119: ANT_SW_SEL0 (Follow QCT) */
 
 	{ /* NC (PA1_R0) */
 		.gpio = 120,
@@ -755,8 +677,6 @@ static struct msm_gpiomux_config semc_viskan_all_cfgs[] __initdata = {
 	},
 
 	/* 122-123 not configured */
-	/* 122: PA0_R0 (Follow QCT) */
-	/* 123: PA0_R1 (Follow QCT) */
 
 	{ /* NC */
 		.gpio = 124,
@@ -764,16 +684,6 @@ static struct msm_gpiomux_config semc_viskan_all_cfgs[] __initdata = {
 	},
 
 	/* 125-134 not configured */
-	/* 125: WTR0_RF_ON (Follow QCT) */
-	/* 126: WTRO_RX_ON (Follow QCT) */
-	/* 127: APT0_VCON (Follow QCT) */
-	/* 128: PA_ON8 (Follow QCT) */
-	/* 129: PA_ON7 (Follow QCT) */
-	/* 130: PA_ON6 (Follow QCT) */
-	/* 131: PA_ON5 (Follow QCT) */
-	/* 132: PA_ON4 (Follow QCT) */
-	/* 133: PA_ON3 (Follow QCT) */
-	/* 134: PA_ON2 (Follow QCT) */
 
 	{ /* NC */
 		.gpio = 135,
@@ -781,8 +691,6 @@ static struct msm_gpiomux_config semc_viskan_all_cfgs[] __initdata = {
 	},
 
 	/* 136-137 not configured */
-	/* 136: PA_ON0 (Follow QCT) */
-	/* 137: GPS_LNA_EN (Follow QCT) */
 
 	{ /* NC */
 		.gpio = 138,
@@ -790,7 +698,6 @@ static struct msm_gpiomux_config semc_viskan_all_cfgs[] __initdata = {
 	},
 
 	/* 139 not configured */
-	/* 139: DRX_SW_SEL0 (Follow QCT) */
 
 	{ /* NC */
 		.gpio = 140,
@@ -802,22 +709,17 @@ static struct msm_gpiomux_config semc_viskan_all_cfgs[] __initdata = {
 	},
 
 	/* 142-143 not configured */
-	/* 142: WTR0_SSBI_PRX_DRX (Follow QCT) */
-	/* 143: WTR0_SSBI_TX_GNSS (Follow QCT) */
 
 	{ /* NC */
 		.gpio = 144,
-		.settings = { [GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low, },
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
 	{ /* NC */
 		.gpio = 145,
-		.settings = { [GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low, },
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
 
 	/* 146-148 not configured */
-	/* 146: WTR0 GPDATA2 (Follow QCT) */
-	/* 147: WTR0 GPDATA1 (Follow QCT) */
-	/* 148: WTR0 GPDATA0 (Follow QCT) */
 
 	{ /* NC */
 		.gpio = 149,
@@ -825,20 +727,74 @@ static struct msm_gpiomux_config semc_viskan_all_cfgs[] __initdata = {
 	},
 	{ /* NC */
 		.gpio = 150,
-		.settings = { [GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low, },
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
 	{ /* NC */
 		.gpio = 151,
-		.settings = { [GPIOMUX_SUSPENDED] = &gpio_2ma_no_pull_low, },
+		.settings = { [GPIOMUX_SUSPENDED] = &unused_gpio, },
 	},
 };
 
 
+static struct gpiomux_setting wcnss_5wire_suspend_cfg = {
+	.func = GPIOMUX_FUNC_GPIO,
+	.drv  = GPIOMUX_DRV_2MA,
+	.pull = GPIOMUX_PULL_UP,
+};
+
+static struct gpiomux_setting wcnss_5wire_active_cfg = {
+	.func = GPIOMUX_FUNC_1,
+	.drv  = GPIOMUX_DRV_6MA,
+	.pull = GPIOMUX_PULL_DOWN,
+};
+
+struct msm_gpiomux_config wcnss_5wire_interface[] = {
+	{
+		.gpio = 84,
+		.settings = {
+			[GPIOMUX_ACTIVE]    = &wcnss_5wire_active_cfg,
+			[GPIOMUX_SUSPENDED] = &wcnss_5wire_suspend_cfg,
+		},
+	},
+	{
+		.gpio = 85,
+		.settings = {
+			[GPIOMUX_ACTIVE]    = &wcnss_5wire_active_cfg,
+			[GPIOMUX_SUSPENDED] = &wcnss_5wire_suspend_cfg,
+		},
+	},
+	{
+		.gpio = 86,
+		.settings = {
+			[GPIOMUX_ACTIVE]    = &wcnss_5wire_active_cfg,
+			[GPIOMUX_SUSPENDED] = &wcnss_5wire_suspend_cfg,
+		},
+	},
+	{
+		.gpio = 87,
+		.settings = {
+			[GPIOMUX_ACTIVE]    = &wcnss_5wire_active_cfg,
+			[GPIOMUX_SUSPENDED] = &wcnss_5wire_suspend_cfg,
+		},
+	},
+	{
+		.gpio = 88,
+		.settings = {
+			[GPIOMUX_ACTIVE]    = &wcnss_5wire_active_cfg,
+			[GPIOMUX_SUSPENDED] = &wcnss_5wire_suspend_cfg,
+		},
+	},
+};
+
+const size_t wcnss_5wire_interface_size = ARRAY_SIZE(wcnss_5wire_interface);
 
 void __init gpiomux_device_install(void)
 {
-	msm_gpiomux_install(semc_viskan_all_cfgs,
-			ARRAY_SIZE(semc_viskan_all_cfgs));
+	msm_gpiomux_install(semc_blue_all_cfgs,
+			ARRAY_SIZE(semc_blue_all_cfgs));
+
+	msm_gpiomux_install(wcnss_5wire_interface,
+			ARRAY_SIZE(wcnss_5wire_interface));
 }
 
 int __init msm8960_init_gpiomux(void)
